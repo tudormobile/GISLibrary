49a6e6cf51e7cfea0767435465d4fcb32bcb0dc3
Bill Tudor <bill@tudormobile.com>
2024-02-10 12:20:19 -0500

    repo initial setup

---
+++ src/GISLibrary.Tests/Class1Tests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tudormobile.GISLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tudormobile.GISLibrary.Tests
+{
+    [TestClass()]
+    public class Class1Tests
+    {
+        [TestMethod()]
+        public void addTest()
+        {
+            var target = new Class1();
+            var expected = 3;
+            var actual = target.add(1, 2);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ src/GISLibrary/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tudormobile.GISLibrary
+{
+    /// <summary>
+    /// This is class 1 summary.
+    /// </summary>
+    public class Class1
+    {
+        /// <summary>
+        /// This is class 1 add method.
+        /// </summary>
+        /// <param name="x">First argument.</param>
+        /// <param name="y">Second argument.</param>
+        /// <returns>First plus second.</returns>
+        public int add(int x, int y) => x + y;
+    }
+}
